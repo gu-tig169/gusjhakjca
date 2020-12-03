659fb06ccde38f0b7eca286b8fdb319bafa5be15
kuojinNerverland <gusjhakjca@student.gu.se>
2020-12-03 10:52:32 +0100

    Nätverk klart. Fullt fungerande.

--- lib/Api.dart
+++ lib/Api.dart
@@ -3,13 +3,14 @@ import 'TaskItem.dart';
 import 'package:http/http.dart' as http;
 
 const API_URL = 'https://todoapp-api-vldfm.ondigitalocean.app';
-const API_KEY = '91a17fc2-ec95-4702-9d47-d246df377b3b';
+const API_KEY = '7bed00ca-6040-4be3-b026-c6d354a8fe3b';
 
 class Api {
   // Lägga till data
   static Future addTask(TaskItem task) async {
     Map<String, dynamic> json = TaskItem.toJson(task);
     print(json);
+
     await http.post('$API_URL/todos?key=$API_KEY',
         headers: {'Content-Type': 'application/json'},
         body: jsonEncode(<String, dynamic>{
@@ -36,6 +37,8 @@ class Api {
   }
 
   static Future<List<TaskItem>> getTasks() async {
+    var url = '$API_URL/todos?key=$API_KEY';
+    print(url);
     var response = await http.get('$API_URL/todos?key=$API_KEY');
     String bodyString = response.body;
     print(response.body);
